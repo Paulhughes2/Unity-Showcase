3dae43b13d9e9818908f4c2b3915d7c0554d327a
Paulhughes2 <plhgh118@hotmail.com>
2021-02-12 11:06:41 +0000

    Added AI

---
+++ Assets/AIMove.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIMove : MonoBehaviour
+{
+
+    public Transform target;
+    Vector3 leashOrigin;
+    public float range, leashRange;
+    public float attackRange;
+    NavMeshAgent agent;
+    bool originSet = false;
+    bool hasLOS;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+
+        leashOrigin = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move to mouse click
+        /*
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+
+            if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition),out hit, 100))
+            {
+            
+                agent.SetDestination(hit.point);
+
+            }
+        }*/
+
+        //Move to one static location
+
+        if (Vector3.Distance(transform.position, target.position) <= range && losCheck())
+        {
+            if (!originSet)
+            {
+                leashOrigin = transform.position;
+                originSet = true;
+            }
+
+            if (Vector3.Distance(transform.position, target.position) > attackRange)
+            {
+                agent.isStopped = false;
+                agent.SetDestination(target.position);
+            }
+            else
+            {
+                agent.isStopped = true;
+            }
+
+            if (agent.isStopped == true)
+            {
+
+                if (losCheck())
+                {
+                    Debug.Log("Pew Pew");
+                }
+            }
+        }
+        else if (Vector3.Distance(transform.position, leashOrigin) > leashRange && agent.destination != target.position)
+        {
+            agent.SetDestination(leashOrigin);
+        }
+        else
+        {
+            agent.isStopped = false;
+            if (Vector3.Distance(transform.position, target.position) < .5)
+            {
+                agent.SetDestination(leashOrigin);
+            }
+            else if (Vector3.Distance(transform.position, leashOrigin) < .5)
+            {
+                agent.SetDestination(target.position);
+
+            }
+
+        }
+    }
+
+
+    bool losCheck()
+    {
+        transform.LookAt(target.position, transform.up);
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, transform.forward, out hit, 100))
+        {
+            if (hit.transform.gameObject.tag == "Player")
+            {
+                hasLOS = true;
+            }
+        }
+        else
+        {
+            hasLOS = false;
+        }
+        return hasLOS;
+    }
+}
--- Assets/Scripts/Shooting/ShootGun.cs
+++ Assets/Scripts/Shooting/ShootGun.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class ShootGun : MonoBehaviour
 {
+
+    public AudioSource AS;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +17,9 @@ public class ShootGun : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
+
+            AS.Play();
+
             RaycastHit hit;
 
             if (Physics.Raycast(transform.position, transform.up, out hit, 100))
@@ -27,7 +32,10 @@ public class ShootGun : MonoBehaviour
                 }
             }
         }
-
+        else
+        {
+            AS.Stop();
+        }
         //Debug.DrawRay(transform.position, transform.up, Color.green, 100);
 
     }
