dc635e3a08bd0c3c78d578269b435a0487c4959d
Paulhughes2 <plhgh118@hotmail.com>
2021-03-03 10:05:56 +0000

    Added Teleportation of objects

---
+++ Assets/Scripts/Teleporting/Teleporting.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporting : MonoBehaviour
+{
+
+    public GameObject sphere, cube, cylinder;
+    Vector3 offSet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetOffset();
+        cylinder.transform.position = gameObject.transform.position + offSet;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SetOffset();
+            cube.transform.position = transform.position + offSet;
+        }
+    }
+    void SetOffset()
+    {
+        offSet = new Vector3(Random.Range(-1, -2), 0, Random.Range(-1, -2));
+    }
+}
---
+++ Assets/Scripts/Teleporting/lerp.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lerp : MonoBehaviour
+{
+
+    public Transform endMarker;
+    //Transform startMarker;
+    public float speed = 0.1f;
+    float startTime, journeyLength;
+    bool targetSet = false;
+    Vector3 target;
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        startTime = Time.time;
+
+        journeyLength = Vector3.Distance(transform.position, endMarker.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, endMarker.position) > 1.5 )
+        {
+            if (!targetSet)
+            {
+            target = endMarker.position;
+                targetSet = true;
+            }
+            //Inverted LookAT
+            // transform.LookAt(2 * transform.position - endMarker.position);
+            //Normal LookAT
+            transform.LookAt(target);
+
+            float distCovered = (Time.time - startTime) * speed;
+
+            float fractOfJourney = distCovered / journeyLength;
+
+            transform.position = Vector3.Lerp(transform.position, target, fractOfJourney);
+        }
+       if(Vector3.Distance(transform.position,target)<1)
+        {
+            targetSet = false;
+        }
+    }
+}
